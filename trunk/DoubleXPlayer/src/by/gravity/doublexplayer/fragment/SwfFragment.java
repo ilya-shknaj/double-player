e985d634168e9896aa4b4634d129873e48c8c092
ilya.shknaj <ilya.shknaj@b7b4060e-b81e-a405-c61e-bcfa7256ab33>
2013-09-17 22:29:00 +0000

    update swf fragment added zoom on swf button 35dp

--- trunk/DoubleXPlayer/src/by/gravity/doublexplayer/fragment/SwfFragment.java
+++ trunk/DoubleXPlayer/src/by/gravity/doublexplayer/fragment/SwfFragment.java
@@ -3,7 +3,6 @@ package by.gravity.doublexplayer.fragment;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.Fragment;
@@ -11,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 import by.gravity.doublexplayer.R;
@@ -48,10 +49,11 @@ public class SwfFragment extends Fragment implements IVideo {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		initWebView();
-		initProgressButtons();
+		initButtons();
+		initFlash(getMediaUri());
 	}
 
-	private void initProgressButtons() {
+	private void initButtons() {
 		mPlayButton = (Button) getView().findViewById(R.id.playButton);
 		mPlayButton.setOnClickListener(new OnClickListener() {
 
@@ -76,22 +78,77 @@ public class SwfFragment extends Fragment implements IVideo {
 			}
 		});
 
+		Button zoomInButton = (Button) getView().findViewById(R.id.zoomIn);
+		zoomInButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				zoomIn();
+			}
+		});
+
+		Button zoomOutButton = (Button) getView().findViewById(R.id.zoomOut);
+		zoomOutButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				zoomOut();
+			}
+		});
+
 		setPlayPauseButtonUI();
 	}
 
 	private void initWebView() {
 		mWebView = (WebView) getView().findViewById(R.id.webView);
-
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.getSettings().setAllowFileAccess(true);
+		mWebView.setScrollBarStyle(0);
 		mWebView.getSettings().setPluginsEnabled(true);
-		mWebView.getSettings().setSupportZoom(false);
-		mWebView.getSettings().setAppCacheEnabled(false);
-		mWebView.getSettings().setAllowContentAccess(true);
-		mWebView.getSettings().setCacheMode(
-				mWebView.getSettings().LOAD_NO_CACHE);
-		mWebView.setBackgroundColor(Color.BLACK);
-		mWebView.loadUrl(getMediaUri());
+		mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+		mWebView.setWebChromeClient(new WebChromeClient());
+		// webView.addJavascriptInterface(new CallJava(), "CallJava");
+	}
+
+	private void initFlash(String filePath) {
+		StringBuffer localStringBuffer = new StringBuffer();
+		localStringBuffer
+				.append("<style type='text/css'>html,body,object{ margin: 0px; padding: 0px; border: 0px; width: 100%; height: 100%; overflow: hidden; background-color: #000000; color: #FF0000;} </style>");
+		localStringBuffer
+				.append("<object id='flashmovie' type='application/x-shockwave-flash' align='center' width='100%' height='100%' >");
+		localStringBuffer.append("<param name='movie' value='" + filePath
+				+ "'/>");
+		localStringBuffer.append("<param name='quality' value='medium'/>");
+		localStringBuffer
+				.append("<param name='allowFullScreen' value='false'/>");
+		localStringBuffer.append("<param name='wmode' value='direct'/>");
+		localStringBuffer.append("<param name='scale' value='showall'/>");
+		localStringBuffer
+				.append("<param name='allowScriptAccess' value='always'/>");
+		localStringBuffer.append("<center>");
+		localStringBuffer
+				.append("<div style='border:2px solid #666; background:#EEEEE0; position: relative; top: 100px; '>");
+		localStringBuffer
+				.append("<h3 style='color:red; text-align:center'>Please Install Adobe Flash Plugin and Restart !</h3>");
+		localStringBuffer
+				.append("<h5 style='color:#80BFFF; text-align:center'>(Some Android Devices(CPU Lower ARMv7) Can't Support Flash !)</h5>");
+		localStringBuffer
+				.append("<a href='https://market.android.com/details?id=com.adobe.flashplayer'>DOWNLOAD FROM MARKET</a>");
+		localStringBuffer.append("</div>");
+		localStringBuffer.append("</center>");
+		localStringBuffer.append("</object>");
+		localStringBuffer.append("<script type='text/javascript'>");
+		localStringBuffer
+				.append("function GetFlashInfo(){var total_frame = flashmovie.TotalFrames();var currt_frame = flashmovie.CurrentFrame();CallJava.SetFlashInfo(total_frame,currt_frame);}");
+		localStringBuffer.append("function Play(){flashmovie.Play();}");
+		localStringBuffer.append("function Pause(){flashmovie.StopPlay();}");
+		localStringBuffer.append("function ZoomIn(){flashmovie.Zoom(90);}");
+		localStringBuffer.append("function ZoomOut(){flashmovie.Zoom(110);}");
+		localStringBuffer
+				.append("function GotoFrame(goframe){flashmovie.GotoFrame(goframe);}");
+		localStringBuffer.append("</script>");
+		mWebView.loadDataWithBaseURL(null, localStringBuffer.toString(),
+				"text/html", "utf-8", null);
 	}
 
 	@Override
@@ -109,13 +166,21 @@ public class SwfFragment extends Fragment implements IVideo {
 	public void play() {
 		isPlayed = true;
 		setPlayPauseButtonUI();
-		callHiddenWebViewMethod("onResume");
+		mWebView.loadUrl("javascript:Play()");
 	}
 
 	public void pause() {
 		isPlayed = false;
 		setPlayPauseButtonUI();
-		callHiddenWebViewMethod("onPause");
+		mWebView.loadUrl("javascript:Pause()");
+	}
+
+	private void zoomIn() {
+		mWebView.loadUrl("javascript:ZoomIn()");
+	}
+
+	private void zoomOut() {
+		mWebView.loadUrl("javascript:ZoomOut()");
 	}
 
 	private void setPlayPauseButtonUI() {
