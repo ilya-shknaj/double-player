dbf8244545016208cb9931e2a2187ff13e03ded0
ilya.shknaj <ilya.shknaj@b7b4060e-b81e-a405-c61e-bcfa7256ab33>
2013-09-19 17:27:24 +0000

    change format to txt

--- trunk/DoubleXPlayer/src/by/gravity/doublexplayer/util/PlayerUtil.java
+++ trunk/DoubleXPlayer/src/by/gravity/doublexplayer/util/PlayerUtil.java
@@ -5,7 +5,7 @@ public class PlayerUtil {
 	public static String changeFileExtensionToTxt(String path) {
 		int index = path.lastIndexOf(".");
 		if (index != -1) {
-			return path.substring(0, index + 1) + "TXT";
+			return path.substring(0, index + 1) + "txt";
 		}
 
 		return null;
