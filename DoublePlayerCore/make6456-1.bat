bcd27d39f4bdeb092b060024ebbe086ae10b84e3
ilya.shknaj <ilya.shknaj@b7b4060e-b81e-a405-c61e-bcfa7256ab33>
2013-09-23 21:18:52 +0000

    init import

---
+++ DoublePlayerCore/make6456-1.bat
@@ -0,0 +1,2 @@
+@echo off
+D:\Study\Android\Library\gstreamer-sdk-android-arm-debug-2013.6/share/gst-android/ndk-build//tools/windows/sed -n "s/^dependency_libs='\(.*\)'/\1/p" D:/Study/Android/Library/gstreamer-sdk-android-arm-debug-2013.6/lib/libgstinterfaces-0.10.la
