b2f44bcc03c80b8067f44e3252bc512a2c4f7dc3
ilya.shknaj <ilya.shknaj@b7b4060e-b81e-a405-c61e-bcfa7256ab33>
2013-11-16 11:44:59 +0000

    Initial import.

---
+++ DoublePlayerObstacle/trunk/DoublePlayerObstacle/src/by/gravity/doublexplayer/obstacle/activity/MainActivity.java
@@ -0,0 +1,12 @@
+package by.gravity.doublexplayer.obstacle.activity;
+
+import by.gravity.doublexplayer.obstacle.R;
+
+public class MainActivity extends
+		by.gravity.doublexplayer.activity.MainActivity {
+
+	@Override
+	protected int getLayoutBackground() {
+		return R.drawable.bg_obstacle;
+	}
+}
