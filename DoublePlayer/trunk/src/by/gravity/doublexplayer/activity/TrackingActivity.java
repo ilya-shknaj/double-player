86c4b98fe05a9016b89df784d22374f302aaf691
ilya.shknaj <ilya.shknaj@b7b4060e-b81e-a405-c61e-bcfa7256ab33>
2013-12-18 07:11:49 +0000

    add flurry

--- DoublePlayer/trunk/src/by/gravity/doublexplayer/activity/TrackingActivity.java
+++ DoublePlayer/trunk/src/by/gravity/doublexplayer/activity/TrackingActivity.java
@@ -3,6 +3,7 @@ package by.gravity.doublexplayer.activity;
 import android.support.v4.app.FragmentActivity;
 
 import com.bugsense.trace.BugSenseHandler;
+import com.flurry.android.FlurryAgent;
 import com.google.analytics.tracking.android.EasyTracker;
 
 public class TrackingActivity extends FragmentActivity {
@@ -11,11 +12,13 @@ public class TrackingActivity extends FragmentActivity {
 		super.onStart();
 		EasyTracker.getInstance().activityStart(this);
 		BugSenseHandler.initAndStartSession(this, "b646ffe2");
+		FlurryAgent.onStartSession(this, "ZJM6Q6B3KTP5RS38SBPQ");
 	}
 
 	@Override
 	public void onStop() {
 		super.onStop();
 		EasyTracker.getInstance().activityStop(this);
+		FlurryAgent.onEndSession(this);
 	}
 }
